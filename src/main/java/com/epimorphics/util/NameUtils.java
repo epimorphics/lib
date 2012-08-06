0844f4d6613c3774ea404c7e8c1b532e03be4c90
der <dave@epimorphics.com>
2012-08-06 16:54:05 +0100

    Trivial extra uri hacking util (from Guild)

--- src/main/java/com/epimorphics/util/NameUtils.java
+++ src/main/java/com/epimorphics/util/NameUtils.java
@@ -2,7 +2,7 @@
  * File:        NameUtils.java
  * Created by:  Dave Reynolds
  * Created on:  10 Apr 2011
- * 
+ *
  * (c) Copyright 2011, Epimorphics Limited
  *
  *****************************************************************/
@@ -19,52 +19,63 @@ import org.apache.jena.iri.IRIFactory;
 /**
  * Utilities for checking and converting names to forms safely
  * usable in different circumstances.
- * 
+ *
  * @author <a href="mailto:dave@epimorphics.com">Dave Reynolds</a>
  */
 public class NameUtils {
 
     /**
      * Convert an arbitrary name to name safe to use as a file name or URL path slug.
-     * Not reversible. 
+     * Not reversible.
      */
-	public static String safeName(String name) {
+    public static String safeName(String name) {
         return name.replaceAll("[^@a-zA-Z0-9_\\.\\-~]+", "_");
-	}
-	
-	/**
-	 * Convert an arbitrary name to a URI which can be used in metadata
-	 * descriptions of the named object. If the name already looks like
-	 * a URI then use that. If not then mint a URI based on the address of the
-	 * server and the given name of the "type" of resource. This will
-	 * look like:
-	 * <pre>
-	 *  {intendedBaseURL}/modal/{type}/{safe-version-of-name}
-	 * </pre>
-	 */
-	public static String uriFor(String name, String serverURI, String type) {
-		if (isURI(name)) { 
+    }
+
+    /**
+     * Convert an arbitrary name to a URI which can be used in metadata
+     * descriptions of the named object. If the name already looks like
+     * a URI then use that. If not then mint a URI based on the address of the
+     * server and the given name of the "type" of resource. This will
+     * look like:
+     * <pre>
+     *  {intendedBaseURL}/modal/{type}/{safe-version-of-name}
+     * </pre>
+     */
+    public static String uriFor(String name, String serverURI, String type) {
+        if (isURI(name)) {
             return name;
-		} else {
+        } else {
             // not a valid IRI
             return serverURI + "/modal/" + type + "/" + safeName(name);
-		}
-	}
-	
-	/**
-	 * Test if the given name is a legal URI. 
-	 */
-	public static boolean isURI(String name) {
+        }
+    }
+
+    /**
+     * Test if the given name is a legal URI.
+     */
+    public static boolean isURI(String name) {
         IRI testing = IRIFactory.jenaImplementation().create(name);
         return ! testing.hasViolation(false);
-	}
-	
-	/**
-	 * Convert a name to a safe name in a reversible fashion by
-	 * (very) conservative percent-encoding of the UFT-8 version. 
-	 */
-	public static String encodeSafeName(String name) {
-	    try {
+    }
+
+
+    /**
+     * Encode a resource URI into a unique label which can be used
+     * as a URI segment into a REST API. Not reversible.
+     */
+    public static String idFromURI(String uri) {
+        // TODO decide on handling of non-ascii chars in id generation
+        // use _ instead of %-encode because %-encode of "/" causes problems for e.g. Grizzly
+        return uri.replaceFirst("http(s?)://", "").replaceAll("[^-\\.@\\w]", "_");
+    }
+
+    /**
+     * Convert a name to a safe name in a reversible fashion by
+     * (very) conservative percent-encoding of the UFT-8 version.
+     */
+    public static String encodeSafeName(String name) {
+        try {
             StringBuilder encode = new StringBuilder();
             for (byte b : name.getBytes("UTF-8")) {
                 char c = (char)b;
@@ -79,32 +90,32 @@ public class NameUtils {
         } catch (UnsupportedEncodingException e) {
             throw new EpiException(e);
         }
-	}
+    }
 
-	/**
-	 * Decode an encoded safe name
-	 */
-	public static String decodeSafeName(String name) {
-	    byte[] decode = new byte[ name.length() ];
-	    int p = 0;
-	    for (int i = 0; i < name.length(); i++) {
-	    	char c = name.charAt(i);
-	    	if (c == '%') {
-	    		decode[p++] = (byte) Integer.parseInt(name.substring(i+1, i+3), 16);
-	    		i += 2;
-	    	} else {
-	    		decode[p++] = (byte) c;
-	    	}
-	    }
-	    byte[] after = new byte[p];
-	    System.arraycopy(decode, 0, after, 0, p);
-	    try {
+    /**
+     * Decode an encoded safe name
+     */
+    public static String decodeSafeName(String name) {
+        byte[] decode = new byte[ name.length() ];
+        int p = 0;
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (c == '%') {
+                decode[p++] = (byte) Integer.parseInt(name.substring(i+1, i+3), 16);
+                i += 2;
+            } else {
+                decode[p++] = (byte) c;
+            }
+        }
+        byte[] after = new byte[p];
+        System.arraycopy(decode, 0, after, 0, p);
+        try {
             return new String(after, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             throw new EpiException(e);
         }
-	}
-	
+    }
+
     /**
      * Generate a random name for something of the given type, result
      * will currently look like: {type}-{uuid}
@@ -112,7 +123,7 @@ public class NameUtils {
     public static String newName(String type){
         return type + "-" + UUID.randomUUID();
     }
-    
+
     /**
      * Generate a random URI for something of the given type, result
      * will currently look like: {server}/modal/{type}/{uuid}
@@ -120,12 +131,12 @@ public class NameUtils {
     public static String newURI(String type, String serverURI){
         return serverURI + "/modal/" + type + "/" + UUID.randomUUID();
     }
-    
+
    /**
     * Extract an integer from a parameter object, with default
     */
     public static int asInt(Object param, int defaultint) {
-        if (param == null) { 
+        if (param == null) {
             return defaultint;
         }
         if (param instanceof Number) {
@@ -137,7 +148,7 @@ public class NameUtils {
             return defaultint;
         }
     }
-    
+
     /**
      * Normalize a relative file path to unix syntax.
      * I.e. on widows will replace "\" by "/"
@@ -205,5 +216,5 @@ public class NameUtils {
             return filen.substring(0, split);
         }
 
-    }    
+    }
 }
