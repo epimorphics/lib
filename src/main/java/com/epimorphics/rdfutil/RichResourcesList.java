b0dd5d7802592c85fb48fca839c8439b612dc090
Ian Dickinson <i.j.dickinson@gmail.com>
2012-11-21 00:15:40 +0000

    Added simple wrapper for passing around lists of resources with their description model

---
+++ src/main/java/com/epimorphics/rdfutil/RichResourcesList.java
@@ -0,0 +1,124 @@
+/*****************************************************************************
+ * File:    RichResource.java
+ * Project: epimorphics-lib
+ * Created: 20 Nov 2012
+ * By:      ian
+ *
+ * Copyright (c) 2012 Epimorphics Ltd. All rights reserved.
+ *****************************************************************************/
+
+// Package
+///////////////
+
+package com.epimorphics.rdfutil;
+
+
+// Imports
+///////////////
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hp.hpl.jena.rdf.model.*;
+
+/**
+ * <p>A rich resource is an RDF resource with an attached model. A {@link RichResourcesList}
+ * contains an ordered list of resources, with a shared model containing their combined
+ * description.</p>
+ *
+ * @author Ian Dickinson, Epimorphics (mailto:ian@epimorphics.com)
+ */
+@SuppressWarnings( "serial" )
+public class RichResourcesList
+extends ArrayList<Resource>
+{
+    /***********************************/
+    /* Constants                       */
+    /***********************************/
+
+    /***********************************/
+    /* Static variables                */
+    /***********************************/
+
+    @SuppressWarnings( value = "unused" )
+    private static final Logger log = LoggerFactory.getLogger( RichResourcesList.class );
+
+    /***********************************/
+    /* Instance variables              */
+    /***********************************/
+
+    private Model description;
+
+    /***********************************/
+    /* Constructors                    */
+    /***********************************/
+
+    public RichResourcesList() {
+        this( null );
+    }
+
+    public RichResourcesList( Model m ) {
+        this.description = m;
+    }
+
+    /***********************************/
+    /* External signature methods      */
+    /***********************************/
+
+    /**
+     * Add a resource to the list. Add the model that r is attached to to this
+     * list's description model, and ensure that the resource that is added to the
+     * list is attached to the combined model.
+     * @param r The resource to add to the model
+     */
+    @Override
+    public boolean add( Resource r ) {
+        return add( r, r.getModel() );
+    }
+
+    /**
+     * Add a resource to the list, and add the given description model to the combined
+     * description.
+     *
+     * @param r A resource to add
+     * @param rDescription A description model to add
+     * @return
+     */
+    public boolean add( Resource r, Model rDescription ) {
+        addDescription( rDescription );
+        return super.add( r.inModel( description() ) );
+    }
+
+    /**
+     * Add a description model to the combined model for this list.
+     * @param m
+     */
+    public void addDescription( Model m ) {
+        if (m != null) {
+            description().add( m );
+        }
+    }
+
+    /**
+     * Return the combined description model for this list.
+     * @return
+     */
+    public Model description() {
+        if (description == null) {
+            description = ModelFactory.createDefaultModel();
+        }
+        return description;
+    }
+
+    /***********************************/
+    /* Internal implementation methods */
+    /***********************************/
+
+    /***********************************/
+    /* Inner class definitions         */
+    /***********************************/
+
+}
+
---
+++ src/test/java/com/epimorphics/rdfutil/RichResourcesListTest.java
@@ -0,0 +1,158 @@
+/*****************************************************************************
+ * File:    RichResourcesListTest.java
+ * Project: epimorphics-lib
+ * Created: 20 Nov 2012
+ * By:      ian
+ *
+ * Copyright (c) 2012 Epimorphics Ltd. All rights reserved.
+ *****************************************************************************/
+
+// Package
+///////////////
+
+package com.epimorphics.rdfutil;
+
+
+// Imports
+///////////////
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.epimorphics.util.TestUtil;
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.vocabulary.RDF;
+
+/**
+ * <p>TODO class comment</p>
+ *
+ * @author Ian Dickinson, Epimorphics (mailto:ian@epimorphics.com)
+ */
+public class RichResourcesListTest
+{
+    /***********************************/
+    /* Constants                       */
+    /***********************************/
+
+    /***********************************/
+    /* Static variables                */
+    /***********************************/
+
+    @SuppressWarnings( value = "unused" )
+    private static final Logger log = LoggerFactory.getLogger( RichResourcesListTest.class );
+
+    /***********************************/
+    /* Instance variables              */
+    /***********************************/
+
+    /***********************************/
+    /* Constructors                    */
+    /***********************************/
+
+    /***********************************/
+    /* External signature methods      */
+    /***********************************/
+
+    @Test
+    public void testRichResourcesList() {
+        assertNotNull( new RichResourcesList() );
+    }
+
+    @Test
+    public void testRichResourcesListModel() {
+        assertNotNull( new RichResourcesList( ModelFactory.createDefaultModel() ));
+    }
+
+    @Test
+    public void testAddResource() {
+        RichResourcesList rrl = new RichResourcesList();
+        Model m1 = TestUtil.modelFixture( ":a a :b, :c." );
+        Model m2 = TestUtil.modelFixture( ":b a :f. :a a :g." );
+        Resource ra = m1.getResource( TestUtil.baseURIFixture() + "a" );
+        Resource rb = m2.getResource( TestUtil.baseURIFixture() + "b" );
+
+        rrl.add( ra );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "b" ) ) );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "c" ) ) );
+        assertFalse( rrl.get( 0 ).hasProperty( RDF.type, r( "g" ) ) );
+
+        rrl.add( rb );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "b" ) ) );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "c" ) ) );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "g" ) ) );
+
+        assertTrue( rrl.get( 1 ).hasProperty( RDF.type, r( "f" ) ) );
+        assertFalse( rrl.get( 1 ).hasProperty( RDF.type, r( "g" ) ) );
+    }
+
+    @Test
+    public void testAddResourceModel() {
+        RichResourcesList rrl = new RichResourcesList();
+        Model m1 = TestUtil.modelFixture( ":a a :b, :c." );
+        Model m2 = TestUtil.modelFixture( ":b a :f. :a a :g." );
+        Resource ra = ResourceFactory.createResource( TestUtil.baseURIFixture() + "a" );
+        Resource rb = ResourceFactory.createResource( TestUtil.baseURIFixture() + "b" );
+
+        rrl.add( ra, m1 );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "b" ) ) );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "c" ) ) );
+        assertFalse( rrl.get( 0 ).hasProperty( RDF.type, r( "g" ) ) );
+
+        rrl.add( rb, m2 );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "b" ) ) );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "c" ) ) );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "g" ) ) );
+
+        assertTrue( rrl.get( 1 ).hasProperty( RDF.type, r( "f" ) ) );
+        assertFalse( rrl.get( 1 ).hasProperty( RDF.type, r( "g" ) ) );
+    }
+
+    @Test
+    public void testAddDescription() {
+        RichResourcesList rrl = new RichResourcesList();
+        Model m1 = TestUtil.modelFixture( ":a a :b, :c." );
+        Model m2 = TestUtil.modelFixture( ":b a :f. :a a :g." );
+        Resource ra = ResourceFactory.createResource( TestUtil.baseURIFixture() + "a" );
+
+        rrl.add( ra, m1 );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "b" ) ) );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "c" ) ) );
+        assertFalse( rrl.get( 0 ).hasProperty( RDF.type, r( "g" ) ) );
+
+        rrl.addDescription( m2 );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "b" ) ) );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "c" ) ) );
+        assertTrue( rrl.get( 0 ).hasProperty( RDF.type, r( "g" ) ) );
+    }
+
+    @Test
+    public void testDescription() {
+        RichResourcesList rrl = new RichResourcesList();
+        Model m1 = TestUtil.modelFixture( ":a a :b, :c." );
+        Model m2 = TestUtil.modelFixture( ":b a :f. :a a :g." );
+        Resource ra = m1.getResource( TestUtil.baseURIFixture() + "a" );
+        Resource rb = m2.getResource( TestUtil.baseURIFixture() + "b" );
+
+        rrl.add( ra );
+        assertEquals( 2, rrl.description().size() );
+        rrl.add( rb );
+        assertEquals( 4, rrl.description().size() );
+    }
+
+    /***********************************/
+    /* Internal implementation methods */
+    /***********************************/
+
+    private Resource r( String local ) {
+        return ResourceFactory.createResource( TestUtil.baseURIFixture() + local );
+    }
+
+    /***********************************/
+    /* Inner class definitions         */
+    /***********************************/
+
+}
+
