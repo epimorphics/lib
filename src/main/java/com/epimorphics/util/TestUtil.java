9b1e43152558a78474b80e17801cdcd90962d650
der <dave@epimorphics.com>
2013-02-01 12:43:09 +0000

    Undo last change, other code assumes the full strange signature

--- src/main/java/com/epimorphics/util/TestUtil.java
+++ src/main/java/com/epimorphics/util/TestUtil.java
@@ -96,13 +96,14 @@ public class TestUtil {
 
     /**
      * Return a property with the given URI.
+     * @param m Optional model. If null, the property will be created using the {@link ResourceFactory}
      * @param uri Resource URI. If the URI starts with <code>http:</code>, it will be left intact otherwise
      * it is assumed relative to the {@link #baseURIFixture()}
      * @return A property
      */
-    public static Property propertyFixture( String uri ) {
+    public static Property propertyFixture( Model m, String uri ) {
         String u = uri.startsWith( "http:" ) ? uri : (baseURIFixture() + uri);
-        return ResourceFactory.createProperty( u ) ;
+        return (m == null) ? ResourceFactory.createProperty( u ) : m.getProperty( u );
     }
 
     /**
