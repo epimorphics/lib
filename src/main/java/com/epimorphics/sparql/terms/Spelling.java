cc783de13eb140cf4e1c24eeddf5881ca2798272
Chris Dollin <ehog.hedge@googlemail.com>
2015-09-30 12:00:17 +0100

    Abstracted Spelling out of various classes.
    Not entirely sure about this, but it means code for initialising
    and getting spelling of items with primary text is shared.

---
+++ src/main/java/com/epimorphics/sparql/terms/Spelling.java
@@ -0,0 +1,19 @@
+/*                                                                                                                            
+    LICENCE summary to go here.                                                                                        
+    
+    (c) Copyright 2014 Epimorphics Limited
+*/
+package com.epimorphics.sparql.terms;
+
+public class Spelling {
+
+	protected final String spelling;
+	
+	public Spelling(String spelling) {
+		this.spelling = spelling;
+	}
+	
+	public String getSpelling() {
+		return spelling;
+	}
+}
