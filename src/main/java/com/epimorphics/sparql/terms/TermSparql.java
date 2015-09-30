4e504d20cc47e49f9ed9d32527a9ab742f56f7e8
Chris Dollin <ehog.hedge@googlemail.com>
2015-09-30 11:36:48 +0100

    First tests for template substitutions.

---
+++ src/main/java/com/epimorphics/sparql/terms/TermSparql.java
@@ -0,0 +1,12 @@
+/*                                                                                                                            
+    LICENCE summary to go here.                                                                                        
+    
+    (c) Copyright 2014 Epimorphics Limited
+*/
+package com.epimorphics.sparql.terms;
+
+public interface TermSparql {
+	
+	public void toSparql(StringBuilder sb);
+
+}
