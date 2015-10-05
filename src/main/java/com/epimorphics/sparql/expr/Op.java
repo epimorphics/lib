dc9d42558858c68368bd5954dff1d90f81635179
Chris Dollin <ehog.hedge@googlemail.com>
2015-10-05 14:45:29 +0100

    Added operator type and precedence to built-in Op declarations.

--- src/main/java/com/epimorphics/sparql/expr/Op.java
+++ src/main/java/com/epimorphics/sparql/expr/Op.java
@@ -7,38 +7,56 @@ package com.epimorphics.sparql.expr;
 
 public class Op {
 
-	static final Op fnSTR = new Op("STR");
-	static final Op fnLANG = new Op("LANG");
-	static final Op fnLANGMATCHES = new Op("LANGMATCHES");
-	static final Op fnDATATYPE = new Op("DATATYPE");
-	static final Op fnBOUND = new Op("BOUND");
-	static final Op fnSameTerm = new Op("sameTerm");
-	static final Op fnIsIRI= new Op("isIRI");
-	static final Op fnIsURI = new Op("isURI");
-	static final Op fnIsBlank = new Op("isBlank");
-	static final Op fnIsLiteral = new Op("isLiteral");
+   public static enum OpType {FUNC, INFIX, PREFIX}
+
+	public static final Op fnSTR = new Op(OpType.FUNC, "STR");
+	public static final Op fnLANG = new Op(OpType.FUNC, "LANG");
+	public static final Op fnLANGMATCHES = new Op(OpType.FUNC, "LANGMATCHES");
+	public static final Op fnDATATYPE = new Op(OpType.FUNC, "DATATYPE");
+	public static final Op fnBOUND = new Op(OpType.FUNC, "BOUND");
+	public static final Op fnSameTerm = new Op(OpType.FUNC, "sameTerm");
+	public static final Op fnIsIRI= new Op(OpType.FUNC, "isIRI");
+	public static final Op fnIsURI = new Op(OpType.FUNC, "isURI");
+	public static final Op fnIsBlank = new Op(OpType.FUNC, "isBlank");
+	public static final Op fnIsLiteral = new Op(OpType.FUNC, "isLiteral");
 	
-	static final Op opPlus = new Op("+");
-	static final Op opMinus = new Op("-");
-	static final Op opDiv = new Op("/");
-	static final Op opMul = new Op("*");
-	static final Op opLessEq = new Op("<=");
-	static final Op opGreaterEq= new Op(">=");
-	static final Op opLess = new Op("<");
-	static final Op opGreater = new Op(">");
-	static final Op opEq = new Op("=");
-	static final Op opNe = new Op("!=");
-	static final Op opAnd = new Op("&&");
-	static final Op opOr = new Op("||");
+	public static final Op opDiv = new Op(OpType.INFIX, 1, "/");
+	public static final Op opMul = new Op(OpType.INFIX, 1, "*");
 	
-	static final Op opUnaryPlus = new Op("+");
-	static final Op opUnaryMinus = new Op("-");
-	static final Op opNot = new Op("!");
+	public static final Op opPlus = new Op(OpType.INFIX, 2, "+");
+	public static final Op opMinus = new Op(OpType.INFIX, 2, "-");
+	
+	public static final Op opLessEq = new Op(OpType.INFIX, 3, "<=");
+	public static final Op opGreaterEq= new Op(OpType.INFIX, 3, ">=");
+	public static final Op opLess = new Op(OpType.INFIX, 3, "<");
+	public static final Op opGreater = new Op(OpType.INFIX, 3, ">");
+	
+	public static final Op opEq = new Op(OpType.INFIX, 4, "=");
+	public static final Op opNe = new Op(OpType.INFIX, 4, "!=");
+	
+	public static final Op opAnd = new Op(OpType.INFIX, 5, "&&");
+	public static final Op opOr = new Op(OpType.INFIX, 5, "||");
+	
+	public static final Op opUnaryPlus = new Op(OpType.PREFIX, "+");
+	public static final Op opUnaryMinus = new Op(OpType.PREFIX, "-");
+	public static final Op opNot = new Op(OpType.PREFIX, "!");
 	
 	final String name;
+	final OpType type;
+	final int precedence;
 	
 	public Op(String name) {
+		this(OpType.FUNC, name);
+	}
+	
+	public Op(OpType type, String name) {
+		this(type, 0, name);
+	}
+	
+	public Op(OpType type, int precedence, String name) {
 		this.name = name;
+		this.type = type;
+		this.precedence = precedence;
 	}
 
 	public String getName() {
