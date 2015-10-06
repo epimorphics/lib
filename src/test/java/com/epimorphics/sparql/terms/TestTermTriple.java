6158731fae520592a37b106bfae33faa4b4331b8
Chris Dollin <ehog.hedge@gmail.com>
2015-10-06 14:13:47 +0100

    Simplified triple construction prior to adding property paths.

--- src/test/java/com/epimorphics/sparql/terms/TestTermTriple.java
+++ src/test/java/com/epimorphics/sparql/terms/TestTermTriple.java
@@ -10,35 +10,34 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import com.epimorphics.sparql.templates.Settings;
+import com.epimorphics.test.utils.SparqlUtils;
 
 public class TestTermTriple {
 
+	static final TermURI type = new TermURI("http://example.com/type/T");
+	static final TermURI S = new TermURI("http://example.com/S");
+	static final TermURI P = new TermURI("http://example.com/P");
+
+	static final TermLiteral O = new TermLiteral("chat", type, "");
+
+	static final TermTriple SPO = new TermTriple(S, P, O);
+
 	@Test public void testTripleConstruction() {
-		TermURI type = new TermURI("http://example.com/type/T");
-		TermURI S = new TermURI("http://example.com/S");
-		TermURI P = new TermURI("http://example.com/P");
-		TermLiteral O = new TermLiteral("chat", type, "");
-		TermTriple t1 = new TermTriple(S, P, O);
 		
-		assertSame(S, t1.getS());
-		assertSame(P, t1.getP());
-		assertSame(O, t1.getO());
+		assertSame(S, SPO.getS());
+		assertSame(P, SPO.getP());
+		assertSame(O, SPO.getO());
 		
-		assertEquals(t1, new TermTriple(S, P, O));
+		assertEquals(SPO, new TermTriple(S, P, O));
 	}
 	
 	@Test public void testTripleToSparql() {
-		TermURI type = new TermURI("http://example.com/type/T");
-		TermURI S = new TermURI("http://example.com/S");
-		TermURI P = new TermURI("http://example.com/P");
-		TermLiteral O = new TermLiteral("chat", type, "");
-		TermTriple t1 = new TermTriple(S, P, O);
-		
-		StringBuilder sb = new StringBuilder();
-		Settings s = new Settings();
-		t1.toSparql(s, sb);
-		String result = sb.toString();
-		
+		String result = SparqlUtils.renderToSparql(SPO);
 		assertEquals("<http://example.com/S> <http://example.com/P> 'chat'^^<http://example.com/type/T> .", result);
 	}
+	
+	@Test public void testPropertyPathsToSparql() {
+		
+	}
+	
 }
