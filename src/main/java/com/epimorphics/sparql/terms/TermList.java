a24d96f08b7bd34bba0e237b065847a84b2ca6a7
Chris Dollin <ehog.hedge@gmail.com>
2015-10-23 16:36:53 +0100

    Added terms representing SPARQL lists.

---
+++ src/main/java/com/epimorphics/sparql/terms/TermList.java
@@ -0,0 +1,33 @@
+/*                                                                                                                            
+    LICENCE summary to go here.                                                                                        
+    
+    (c) Copyright 2014 Epimorphics Limited
+*/
+
+package com.epimorphics.sparql.terms;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.epimorphics.sparql.graphpatterns.PatternCommon;
+import com.epimorphics.sparql.templates.Settings;
+
+public class TermList implements PatternCommon, IsSparqler {
+
+	final List<TermAtomic> terms;
+	
+	public TermList(TermAtomic ... terms) {
+		this.terms = Arrays.asList(terms);
+	}
+	
+	@Override public void toSparql(Settings s, StringBuilder sb) {
+		sb.append("(");
+		String before = "";
+		for (TermAtomic t: terms) {
+			sb.append(before); before = " ";
+			t.toSparql(s, sb);
+		}
+		sb.append(")");
+	}
+	
+}
