a0fa07671cef1cad2a3ce5433116acd53161820c
Chris Dollin <ehog.hedge@googlemail.com>
2015-11-23 14:22:56 +0000

    Added blank terms.
    When creating a blank term a string may be passed, in which
    case that string is the id of the node. If no string is passed,
    then Jena's NodeFactory is used to vreate a blank Jena node
    and that nodes id string is used as the term'a label.

---
+++ src/test/java/com/epimorphics/sparql/terms/TestTermBlank.java
@@ -0,0 +1,32 @@
+/*                                                                                                                            
+    LICENCE summary to go here.                                                                                        
+    
+    (c) Copyright 2014 Epimorphics Limited
+*/
+package com.epimorphics.sparql.terms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.epimorphics.util.SparqlUtils;
+
+import static com.epimorphics.util.Asserts.*;
+
+public class TestTermBlank {
+	
+	@Test public void testBlankTerms() {
+		Blank ba = new Blank("ziggy"), bb = new Blank("ziggy");
+		Blank c = new Blank("soggy");
+		assertEquals(ba, bb);
+		assertDiffer(c, ba);
+		Blank b1 = new Blank(), b2 = new Blank();
+		assertDiffer(b1, b2);
+	}
+	
+	@Test public void testBlankRendering() {
+		Blank b = new Blank("left");
+		assertEquals("_:left", SparqlUtils.renderToSparql(b));
+				
+	}
+}
